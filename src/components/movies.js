455d29358153c985a4ae62c9a37bcb1466d1ed89
Julien Lavie <jlavie@leadformance.com>
2021-02-22 16:29:52 +0100

    display list of movies

---
+++ src/components/movies.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+    state = {
+        movies: getMovies()
+    };
+
+    handleDelete = (movie) => {
+        const movies = [...this.state.movies];
+        const i = movies.findIndex(m => m._id === movie._id);
+
+        movies.splice(i, 1);
+        this.setState({movies});
+    }
+
+    render() {
+        if (this.state.movies.length === 0) return <p>Pas de film dans la base de données</p>;
+
+        return (
+            <React.Fragment>
+                <p>Il y a {this.state.movies.length} films dans la base de données</p>
+                <div className="table-responsive">
+                    <table className="table table-striped table-sm">
+                        <thead>
+                        <tr>
+                            <th>Titre</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Note</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.movies.map(movie =>
+                            <tr key={movie._id}>
+                                <td>{movie.title}</td>
+                                <td>{movie.genre.name}</td>
+                                <td>{movie.numberInStock}</td>
+                                <td>{movie.dailyRentalRate}</td>
+                                <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger">Supprimer</button></td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </table>
+                </div>
+            </React.Fragment>
+        )
+
+    }
+}
+
+export default Movies;
